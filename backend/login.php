8ed56ee9de96c10e6b60eebf28c64fdf392f2317
TuskyTO <elliotbgold@gmail.com>
2025-03-06 09:14:30 -0500

    adding registration stuff (Login page)

--- backend/login.php
+++ backend/login.php
@@ -33,9 +33,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 $conn->close();
 ?>
 
-<!-- Minimal HTML form for login -->
+<title>
+
+    Login
+
+</title>
+
+
 <form method="POST" action="login.php">
   <input type="text" name="username" placeholder="Username" required>
   <input type="password" name="password" placeholder="Password" required>
   <button type="submit">Login</button>
 </form>
+
+<body>
+    If you do not already have an account hre
+    <p>If you do not already have an account <a href='register.php'>Click here to Register</a> 
+</p>
+
+<body>
+    
